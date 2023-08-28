cc444b5e400119264cc8ecfc5ad4d817658f5fa8
Daniel Honus <dhonus@tuta.io>
2023-08-28 19:54:38 +0200

    new backbone

--- src/routes/blog/+page.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-export const load = async ({ fetch }:any) => {
-  const res = await fetch(`/api?start=0&end=1`);
-
-  const items = await res.json();
-  console.log(items);
-
-  return {items};
-}
