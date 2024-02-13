f507688e248c4576a8041c24467076b2a2374839
dhonus <dhonus@tuta.io>
2024-02-13 20:15:36 +0100

    removed the old stuff

--- src/routes/+page.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-export const load = async ({ fetch }:any) => {
-  const res = await fetch(`/api?limit=5`)
-  const items = await res.json();
-
-  return {items};
-}
-export const prerender = true
